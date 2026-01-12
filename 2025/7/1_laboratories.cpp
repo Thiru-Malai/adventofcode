defb2b45c0d5aad8b41d4b9362e2c8ea7f1ad235
Thirumalai <malaithiru370@gmail.com>
2026-01-12 23:30:28 +0530

    day_7_1_2025_solving

---
+++ 2025/7/1_laboratories.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    ifstream file("input.txt");
+    ifstream file2("example_input.txt");
+    
+    string line;
+    unsigned long long result = 0;
+    
+    while(getline(file2, line)){
+        
+    }
+}
