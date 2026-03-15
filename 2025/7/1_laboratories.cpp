76b2ceb7ccd612622b9860095de5aa921e95ad89
Thirumalai <malaithiru370@gmail.com>
2026-03-15 08:56:48 +0530

    day_7_1_2025_solved_cleared_comments

--- 2025/7/1_laboratories.cpp
+++ 2025/7/1_laboratories.cpp
@@ -12,7 +12,6 @@ int main() {
   unsigned long long result = 0;
   int s_pos = 0, lineCount = 0, rowLength = 0;
   vector<vector<char>> manifold;
-  vector<int> splitter_pos;
   char new_manifold[300][300];
   
   for(int i = 0; i < 300; i++){
@@ -29,57 +28,25 @@ int main() {
         if (line[i] == 'S') {
             new_manifold[lineCount][i] = 'S';
             s_pos = i;
-          // splitter_pos.push_back(i);
         }
       } else {
           if(line[i] == '.' && lineCount > 0 && (new_manifold[lineCount-1][i] == 'S' || new_manifold[lineCount-1][i] == '|')){
               line[i] = '|';
               new_manifold[lineCount][i] = '|';
           } else if(line[i] == '^' && (new_manifold[lineCount-1][i] == '|')){
-              // if(new_manifold[lineCount-1][i-1] != '|' || new_manifold[lineCount-1][i+1] != '|'){
               result++;
-              // }
               new_manifold[lineCount][i-1] = '|';
               new_manifold[lineCount][i+1] = '|';
               new_manifold[lineCount][i] = '^';
           } else if(line[i] == '^' && (new_manifold[lineCount-1][i] != '|')){
               new_manifold[lineCount][i] = '^';
           }
-
-        // if (line[i] == '.'){
-        //     for(int j = 0; j < splitter_pos.size(); j++){
-        //         if(splitter_pos[j] == i){
-        //             line[i] = '|';
-        //         }
-        //     }
-        // } else if(line[i] == '^'){
-        //     for(int j = 0; j < splitter_pos.size(); j++){
-        //         if(splitter_pos[j] == i){
-        //             line[i-1] = '|';
-        //             line[i+1] = '|';
-        //             splitter_pos.push_back(i-1);
-        //             splitter_pos.push_back(i+1);
-        //             // cout<<"Removing "<<splitter_pos.at(i)<<endl;
-        //             splitter_pos.erase(splitter_pos.begin() + j);
-        //             // if()
-        //             // cout<<"Now at "<<splitter_pos.at(i)<<endl;
-        //         }
-        //     }
-        // }
       }
       manifoldRow.push_back(line[i]);
-      // new_manifoldRow[i] = line[i];
     }
-    // new_manifold[lineCount] = new_manifoldRow;
     lineCount++;
     manifold.push_back(manifoldRow);
   }
-  // for(vector<char> itr : manifold){
-  //     for(char itr1 : itr){
-  //         cout<<itr1;
-  //     }
-  //     cout<<endl;
-  // }
   cout<<endl;
   for(int i = 0; i < lineCount; i++){
       for(int j = 0; j < rowLength; j++){
